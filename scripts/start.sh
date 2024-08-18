b20eb95d58fea485e0f2e6cf963a215e5e11fc5d
Esteban MANSART <esteban.mansart@gmail.com>
2024-08-18 23:21:14 +0200

    Test deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,12 @@
 
 # Nom de l'application dans PM2
 APP_NAME="tracrypto"
+SERVER_PATH=~/apps/tracrypto/server
 
 # Vérifier si l'application est déjà en cours d'exécution
 pm2 describe $APP_NAME > /dev/null 2>&1
+
+cd $SERVER_PATH
 
 if [ $? -eq 0 ]; then
     # Si l'application est en cours d'exécution, la redémarrer
@@ -13,5 +16,5 @@ if [ $? -eq 0 ]; then
 else
     # Sinon, démarrer l'application
     echo "Démarrage de l'application $APP_NAME"
-    pm2 start /home/pi/apps/tracrypto/server/index.js --name $APP_NAME
+    pm2 start npm --name $APP_NAME -- run start
 fi
