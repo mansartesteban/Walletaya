85599a40b4b3de47134bd4eea019a48dabfdd62b
Esteban MANSART <esteban.mansart@gmail.com>
2024-08-22 00:08:21 +0200

    Test github actions

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,8 +4,6 @@
 cd ~/apps/Walletaya
 
 # Tirer les dernières modifications depuis GitHub
-# git reset --hard origin/main
-# git pull origin main
 
 # Installer les dépendances (si nécessaire)
 pnpm install
