b4becfe646d51e0eaea2ccff50899519864b63d5
Esteban MANSART <esteban.mansart@gmail.com>
2024-08-21 22:15:03 +0200

    Test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,7 @@ APP_NAME="walletaya"
 # Construire l'image Docker à distance
 docker build -t $APP_NAME .
 
+docker stop $APP_NAME
+
 # Exécuter le conteneur sur l'hôte
 docker run -it -d -p 6000:6000 --rm $APP_NAME 
